590bd4292b5b6fd5398e3e0961cb509e44da7563
basisukh <akalsukhb@gmail.com>
2022-10-05 20:54:26 -0700

    Added AirTag API

---
+++ API/AirTagAPI/Controllers/AirTagsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AirTagAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirTagAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AirTagsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<LongLat> GetLongLat()
+        {
+            var list = new List<LongLat>();
+            string[] lines = System.IO.File.ReadAllLines(@"OriginalAirtag.txt");
+            foreach (string line in lines)
+            {
+                var longLat = line.Split(' ');
+                var longitude = Convert.ToDouble(longLat[0]);
+                var latitude = Convert.ToDouble(longLat[1]);
+                list.Add(new LongLat
+                {
+                    Longitude = longitude,
+                    Latitude = latitude
+                });
+
+            }
+            return list;
+        }
+    }
+}
+
---
+++ API/AirTagAPI/Models/LongLat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AirTagAPI.Models
+{
+    public class LongLat
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+    }
+}
+
