6c9fb7ac79e7146fb3c76a7a69114b31f9f36622
Siddhartha Arora <siddharthaarora@SiddharthasiMac.lan>
2022-10-08 09:54:16 -0700

    Changes to API to add CORS AnyOrigin support and added HTML page with Maps control to fetch locations from API.

--- API/AirTagAPI/Program.cs
+++ API/AirTagAPI/Program.cs
@@ -1,11 +1,22 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins,
+                          policy =>
+                          {
+                              policy.WithOrigins("*")
+                                                  .AllowAnyOrigin()
+                                                  .AllowAnyHeader();
+                          });
+});
+
 
 var app = builder.Build();
 
@@ -21,6 +32,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.Run();
 
